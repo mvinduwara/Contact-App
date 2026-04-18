1b7040fad287f261c146f0c4897adb08181d05ae
mvinduwara <mvinduwara@gmail.com>
2026-04-18 16:09:45 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/my_contact/activity/MainActivity.java
+++ app/src/main/java/com/example/my_contact/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.my_contact.activity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -48,19 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
                 if (itemId == R.id.nav_contacts) {
                     selectedFragment = new ContactsFragment();
-                }
-                else if (itemId == R.id.nav_favourites) {
+                } else if (itemId == R.id.nav_favourites) {
                     selectedFragment = new FavouritesFragment();
-                }
-                else if (itemId == R.id.nav_recents) {
+                } else if (itemId == R.id.nav_recents) {
                     selectedFragment = new RecentsFragment();
-                }
-
-                else if (itemId == R.id.nav_keypad) {
-                    KeypadFragment keypadSheet = new KeypadFragment();
-                    keypadSheet.show(getSupportFragmentManager(), "KeypadBottomSheet");
-
-                    return false;
                 }
 
                 if (selectedFragment != null) {
