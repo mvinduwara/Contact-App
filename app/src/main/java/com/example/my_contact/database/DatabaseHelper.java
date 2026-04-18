02757e5edc77221a22d4b1cf817774db3d736da1
mvinduwara <mvinduwara@gmail.com>
2026-04-18 15:03:14 +0530

    Create DatabaseHelper.java

---
+++ app/src/main/java/com/example/my_contact/database/DatabaseHelper.java
@@ -0,0 +1,47 @@
+package com.example.my_contact.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "contactsApp.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_CONTACTS = "contacts";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_PHONE = "phone";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + TABLE_CONTACTS + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_NAME + " TEXT, "
+                + COLUMN_PHONE + " TEXT)";
+        db.execSQL(createTableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        onCreate(db);
+    }
+
+    public boolean addOne(String name, String phone) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_PHONE, phone);
+
+        long insert = db.insert(TABLE_CONTACTS, null, cv);
+        return insert != -1;
+    }
+}
