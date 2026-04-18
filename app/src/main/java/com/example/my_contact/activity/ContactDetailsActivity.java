a0d6d22cbafeb41fbcee162d935f105c4874cd26
mvinduwara <mvinduwara@gmail.com>
2026-04-18 15:15:16 +0530

    Update ContactDetailsActivity.java

--- app/src/main/java/com/example/my_contact/activity/ContactDetailsActivity.java
+++ app/src/main/java/com/example/my_contact/activity/ContactDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.example.my_contact.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.my_contact.R;
@@ -17,6 +20,8 @@ public class ContactDetailsActivity extends AppCompatActivity {
         TextView tvDetailInitial = findViewById(R.id.tvDetailInitial);
         TextView btnBack = findViewById(R.id.btnBack);
 
+        LinearLayout btnProfileCall = findViewById(R.id.btnProfileCall);
+
         String name = getIntent().getStringExtra("CONTACT_NAME");
         String phone = getIntent().getStringExtra("CONTACT_PHONE");
 
@@ -29,5 +34,13 @@ public class ContactDetailsActivity extends AppCompatActivity {
         }
 
         btnBack.setOnClickListener(v -> finish());
+
+        btnProfileCall.setOnClickListener(v -> {
+            if (phone != null && !phone.isEmpty()) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phone));
+                startActivity(intent);
+            }
+        });
     }
 }
